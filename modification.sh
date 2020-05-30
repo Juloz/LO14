f5c9bd8c6783877ebce507102ddbdde7285ac5d9
jeremielavault <jeremie.lavault@gmail.com>
2020-05-30 21:10:07 +0200

    Add comparison function

---
+++ modification.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Script contenant la fonction modification, piece maitresse du synchroniseur de fichier 
+#$1 Fichier entrain d'être parcouru (path fichier) $2 Nom de l'arbre exploré $3 Path de l'arbre exploré $4 Path de l'autre arbre 
+fonction modification 
+{
+	
+}
